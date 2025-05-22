690401b7ba56f7d9ed75e487e4345db4c6daa82d
Lukesky22sk <imawesomelycool.2011@gmail.com>
2025-05-22 14:02:25 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css"; // Optional: if you're using custom CSS like ChatGPT-style
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
